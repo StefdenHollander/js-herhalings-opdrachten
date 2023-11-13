b1c9ae238780d2149db90fc86acb3d149b836d76
S den Hollander <302045198@student.rocmondriaan.nl>
2023-11-13 10:27:20 +0100

    upd

--- js/main.js
+++ js/main.js
@@ -1,45 +1,114 @@
 console.log("main.js loaded...");
 
-let text = document.querySelector('.show-text');
-let text1 = document.querySelector('.show-text1');
-let text2 = document.querySelector('.show-text2');
+// let text = document.querySelector('.show-text');
+// let text1 = document.querySelector('.show-text1');
+// let text2 = document.querySelector('.show-text2');
 
-let earnings = 2000;
-let raise = 0.05;
 
-let answer = earnings * raise;
+// let earnings = 2000;
+// let raise = 0.05;
 
-text.textContent = answer;
+// let answer = earnings * raise;
+
+// text.textContent = answer;
 
 //Opdracht 2
 
-let price = 400000;
-let interest = 0.025;
+// let price = 400000;
+// let interest = 0.025;
 
-let answer1 = price * interest;
+// let answer1 = price * interest;
 
-text1.textContent = answer1;
+// text1.textContent = answer1;
 
 //opdracht 3
 
-let price1 = 100
-let btw = 1.21
+// let price1 = 100
+// let btw = 1.21
 
-let answer2 = price1 * btw;
+// let answer2 = price1 * btw;
 
-text2.textContent = answer2;
+// text2.textContent = answer2;
 
 //opdracht 4
 
 
-let myName = prompt("type student")
+// let myName = prompt("type student")
 
-if (myName == "student") {
-    console.log("You are a student");
+// if (myName == "student") {
+//     console.log("You are a student");
 
-}
-else {
-    console.log("You are crazy")
-}
+// }
+// else {
+//     console.log("You are crazy")
+// }
 
 //opdracht 5
+
+// const amount = prompt('vul het bedrag in dollars in')
+// const currencyCode = prompt('Vul de currencyCode in')
+
+
+// convertCurrencySwitch(currencyCode, amount);
+// convertCurrencyIfElse(currencyCode, amount);
+
+// function convertCurrencySwitch(currencyCode, amount) {
+
+//     switch (currencyCode) {
+//         case 'btc':
+//             alert(' Het bedrag voor ' + currencyCode + ' in dollars is ' + (amount * 35050))
+//             break;
+
+//         case 'eth':
+//             alert(' Het bedrag voor ' + currencyCode + ' in dollars is ' + (amount * 1887))
+//             break;
+
+//         case 'sol':
+//             alert(' Het bedrag voor ' + currencyCode + ' in dollars is ' + (amount * 40.26))
+//             break;
+
+//         default:
+//             alert('De currencycode ' + currencyCode + ' ken ik niet ')
+//             break;
+//     }
+// }
+
+// function convertCurrencyIfElse(currencyCode, amount) {
+//     if (currencyCode === 'btc') {
+//         alert(' Het bedrag voor ' + currencyCode + ' in dollars is ' + (amount * 35050))
+//     } else if (currencyCode === 'eth') {
+//         alert(' Het bedrag voor ' + currencyCode + ' in dollars is ' + (amount * 1887))
+//     } else
+//         alert('De currencycode ' + currencyCode + ' ken ik niet ')
+// }
+
+//opdracht 6
+
+// for (let index = 0; index < 10; i++) {
+//     console.log(i);   
+//     if(i === 9){
+//         console.log('Einde programma');
+//     }
+// }
+// console.log('Einde programma');
+
+//opdracht 7
+
+let counter = 0;
+while (counter < 20) {
+
+
+
+    console.log(counter + '. dit is zin 1')
+
+
+    if (counter < 10) {
+        console.log(counter + '. dit is zin 2')
+    }
+
+   
+
+
+    counter++;
+}
+console.log('Einde programma');
